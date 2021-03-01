b9897a23eab40916162caf36c452fe5afedda5c3
MonokiniNeko <halasz.gwenael@gmail.com>
2021-03-01 20:47:22 +0100

    issues task and improvement closed

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 function Home({
   onIncrement,
+  onDecrement,
   counter,
 }) {
   return (
     <div>
-      <button>
+      <button onClick={onIncrement}>
         Vous avez cliqué {counter} fois
+      </button>
+      <button onClick={onDecrement}>
+        Annuler un clic
       </button>
     </div>
   );
